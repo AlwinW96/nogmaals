919350f6fe6fee02621ca4f3bf362001a3006add
alwinW96 <99060433@mydavinci.nl>
2021-11-22 09:10:06 +0100

    final edit coins.py

--- coins.py
+++ coins.py
@@ -9,10 +9,7 @@ paid = int(float(input('Paid amount: ')) * 100) #what you paid
 change = paid - toPay #what you paid minus what you have to pay
 
 if change > 0: # if the change is higher than 0
-  coinValue = 0.50 #value of the coin = 0.50 cents
-  coinValue = 1.00 #value of the coin = 1.00 euro
-  coinValue = 2.00 #value of the coin = 2.00 euro
-  coinValue = 5.00 #value of the coin = 5.00 euro
+  coinValue = 500 #value of the coin
   
   while change > 0 and coinValue > 0: #when change and coinvalue is more than 0
     nrCoins = change // coinValue # change and coinvalue equals nrcoins
@@ -23,18 +20,39 @@ if change > 0: # if the change is higher than 0
       change -= nrCoinsReturned * coinValue #CoinsReturned multiplied by CoinValue equals the change
 
 # comment on code below: counting down the value of the coin when returning
-    if coinValue == 5.00:
-      coinValue = 2.00
-    elif coinValue == 2.00:
-      coinValue = 1.00
-    elif coinValue == 1.00:
-      coinValue = 0.50
-    elif coinValue == 0.50:
+    if coinValue == 500:
+      coinValue = 300
+      Returned500coinValue = nrCoinsReturned #remembers the amount of coins you returned
+    elif coinValue == 300:
+      coinValue = 200
+      Returned300coinValue = nrCoinsReturned
+    elif coinValue == 200:
+      coinValue = 100
+      Returned200coinValue = nrCoinsReturned
+    elif coinValue == 100:
+      coinValue = 50
+      Returned100coinValue = nrCoinsReturned
+    elif coinValue == 50:
+      coinValue = 20
+      Returned50coinValue = nrCoinsReturned
+    elif coinValue == 20:
+      coinValue = 10
+      Returned20coinValue = nrCoinsReturned
+    elif coinValue == 5:
       coinValue = 0
+      Returned10coinValue = nrCoinsReturned
     else:
       coinValue = 0
+      Returned5coinValue = nrCoinsReturned
 
 if change > 0: # if change is higher than 0 it prints the change that is not yet returned
   print('Change not returned: ', str(change) + ' cents')
-else:
-  print('done')
+else: #prints the change of specific coins returned
+  print('change of 500 returned:' + str(Returned500coinValue))
+  print('change of 300 returned:' + str(Returned300coinValue))
+  print('change of 200 returned:' + str(Returned200coinValue))
+  print('change of 100 returned:' + str(Returned100coinValue))
+  print('change of 50 returned:' + str(Returned50coinValue))
+  print('change of 20 returned:' + str(Returned20coinValue))
+  print('change of 10 returned:' + str(Returned10coinValue))
+  print('change of 5 returned:' + str(Returned5coinValue))
--- raad-eens/raad-eens.py
+++ raad-eens/raad-eens.py
@@ -1,1 +1,44 @@
 #Alwin Wezenbeek 99060433
+#guessing numbers
+#20 rounds
+#you can guess 10 times each round
+#keep score 1 point for each guessed number to a max of 20 points
+#say the score after each round
+#ask the user to keep playing after each round
+#if the guessed number is closer than 50 numbers say: 'you're getting warmer' 
+#if the number is closer than 20 say:'you're almost burning alive'
+#if the guessed number is further than 50 numbers say: 'you're on room temperature'
+#if the guessed number is further than 100 numbers say: 'you're cold'
+#if the guessed number is further than 200 numbers say: 'you're freezing'
+#after 20 rounds announce the score
+
+#-------Imports-------
+import random
+
+#-------Variables-------
+rounds = 20
+random_number = random.randint(0, 10)
+guesses = 10
+score = 0
+
+#-------Start-Message------
+print('''you're about to guess a number between 1 and 1000
+there are 20 rounds and you can guess a number 10 times each round
+for every guessed number you get 1 point to a max of 20 points
+Good Luck...''')
+
+while guesses != random_number:
+    guesses = 10
+    guess = int(input('guess a number between 1 and 1000: '))
+    guesses -= 1
+    if guess <= random_number:
+        print('number is higher')
+    if guess >= random_number:
+        print('number is lower')
+    if guess == random_number:
+        score += 1
+        print('you guessed it')
+        print ('you have'' ' + str(score) + ' ''points')
+    if guesses >= 0:
+        print('you did not guess it')
+
